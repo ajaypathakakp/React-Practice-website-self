6c915809928984dfdec9fba8c607d0a81dbbdaa3
Ajay Pathak <ajaypathakakp@gmail.com>
2024-01-27 21:56:29 +0530

    first udpate

--- 1_basic_website/src/App.jsx
+++ 1_basic_website/src/App.jsx
@@ -1,19 +1,38 @@
+const reactDescriptions = ["Fundeamental", "Crucial", "core"];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+const randText = reactDescriptions[genRandomInt(2)];
+const randInd = reactDescriptions[randText];
+console.log("Random Index:", randText);
+// I wrote this code above to get the output of Math Number
+function NewFunction() {
+  return (
+    <header>
+      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {randText} React concepts you will need for almost any app you are
+        going to build!
+      </p><br/>
+      <p>{randInd}</p>
+      {/* This line is to understand code */}
+    </header>
+  );
+}
+
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <NewFunction></NewFunction>
+      <genRandomInt />
       <main>
         <h2>Time to get started!</h2>
       </main>
     </div>
   );
 }
-
+// Hello
 export default App;
